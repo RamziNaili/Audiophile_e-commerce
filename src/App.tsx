c7bb23af2ef9d5aae677af09ec5e987569ecc3bd
RamziNaili <naili.ramzi@outlook.fr>
2024-02-07 14:42:46 +0100

    feat: add colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div className="text-green-500">audiophile</div>;
+  return <div className="text-beige">audiophile</div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {},
+      colors: {
+        primary: '#D87D4A',
+        secondary: '#101010',
+        gray: '#F1F1F1',
+        ligthgray: '#FAFAFA',
+        beige: '#FBAF85',
+      },
+    },
   },
   plugins: [],
 };
